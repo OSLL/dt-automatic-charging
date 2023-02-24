af34af03442ba244d1cb8fd3cbab1497772f6af4
KonstantinKondratenko <90711883+KonstantinKondratenko@users.noreply.github.com>
2023-02-24 16:15:40 +0300

    Add circle-ride node

---
+++ dt-automatic-charging/packages/bot_camera/src/circle_drive.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import sys
+import rospy
+from duckietown.dtros import DTROS, NodeType
+from duckietown_msgs.msg import Twist2DStamped
+
+
+# std_msgs/Header header | не передано
+# float32 v              | передано
+# float32 omega          | передано
+
+
+class MyNode(DTROS):
+
+    def __init__(self, node_name):
+        super(MyNode, self).__init__(node_name=node_name, node_type=NodeType.DEBUG)
+        self.pub = rospy.Publisher("~car_cmd", Twist2DStamped, queue_size=1)
+
+    def run(self):
+        rate = rospy.Rate(1)  # 1 message per second
+        while (True):
+            msg = Twist2DStamped()
+            msg.v = 0.1
+            msg.omega = 1.0
+            rospy.loginfo("v;omega as 0.1;1.0")
+            self.pub.publish(msg)
+            rate.sleep()
+            #msg.omega = -1.0
+            #rospy.loginfo("v;omega as 0.1;-1.0")
+            #self.pub.publish(msg)
+            #rate.sleep()
+            sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    # create the node
+    node = MyNode(node_name='bot_camera')
+    # run node
+    node.run()
+    # keep spinning
+    rospy.spin()
+
